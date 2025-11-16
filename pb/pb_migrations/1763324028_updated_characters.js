d546e9ff1ca386337feaa8069452b0d1a407fd95
plw <kwazzartcreature@gmail.com>
2025-11-16 12:14:44 -0800

    schema

---
+++ pb/pb_migrations/1763324028_updated_characters.js
@@ -0,0 +1,39 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3298390430")
+
+  // add field
+  collection.fields.addAt(7, new Field({
+    "hidden": false,
+    "id": "json1874629670",
+    "maxSize": 0,
+    "name": "tags",
+    "presentable": false,
+    "required": false,
+    "system": false,
+    "type": "json"
+  }))
+
+  // add field
+  collection.fields.addAt(8, new Field({
+    "hidden": false,
+    "id": "bool1001664029",
+    "name": "public",
+    "presentable": false,
+    "required": false,
+    "system": false,
+    "type": "bool"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3298390430")
+
+  // remove field
+  collection.fields.removeById("json1874629670")
+
+  // remove field
+  collection.fields.removeById("bool1001664029")
+
+  return app.save(collection)
+})
